b7891d08eac6eb8eebfc3c3abc544cbe7aa03738
inflames32 <cahuzac.p@gmail.com>
2022-11-22 18:53:39 +0100

    separating frontend and backend in differents repositories

--- macollection_backend/app/db/db.js
+++
@@ -1,4 +0,0 @@
-const { Sequelize } = require("sequelize");
-/* const db = new Sequelize("postgres://admin:admin:5432/macollection"); */
-const db = new Sequelize(process.env.POSTGRES_URL_LOCALE);
-module.exports = db;
---
+++ src/Components/CardAlbum.jsx
@@ -0,0 +1,33 @@
+import { useParams } from "react-router-dom";
+import "../Styles/card-album.scss";
+const CardAlbum = (album) => {
+  const { id } = useParams();
+  console.log(album.url);
+  /*  useEffect(() => {
+    getAlbumById(id);
+  }, []); */
+
+  /*   const getAlbumById = async (id) => {
+    await axios
+      .get(`http://localhost:5000/${id}`)
+      .then((res) => {
+        setAlbumsList(res.data.albums);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }; */
+  return (
+    <div className="card">
+      <img src={album.url} alt="" className="card__img" />
+      <div className="card__informations">
+        <p>{album.title}</p>
+        <p>{album.band}</p>
+        <p>{album.year}</p>
+        <p className="card__informations__more">Plus d'infos...</p>
+      </div>
+    </div>
+  );
+};
+export default CardAlbum;
